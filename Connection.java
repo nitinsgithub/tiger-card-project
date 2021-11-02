0c14ffa0b47fd7e05d9405426a95235801e99327
Nitin Shashi <nitin.shashi8@gmail.com>
2021-11-02 10:24:58 +0530

    Java Classes

---
+++ Connection.java
@@ -0,0 +1,17 @@
+package com.mysql.jdbc;
+
+import java.sql.Statement;
+
+public class Connection {
+
+	public boolean isClosed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Statement createStatement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
